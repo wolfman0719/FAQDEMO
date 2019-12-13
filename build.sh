7743b5850e00e914fca8602aa4efe63d52e5e68a
wolfman0719 <hsato0719@icloud.com>
2019-12-13 16:29:38 +0900

    Changes for IRIS 2019.4

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build ./build --no-cache -t wolfman0719/faq:demo
-# docker push wolfman0719/faq:demo
+
+export COMMIT_ID=FAQ demo
+docker-compose build
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-docker run --rm --name myiris -h myiris -p 51773:51773 -p 52773:52773 \
-       wolfman0719/faq:demo
+#!/bin/bash -e
+docker-compose up -d
