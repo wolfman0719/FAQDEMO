e8a02bd5ba0ca5566e0548fe0ca40e0e80b8e561
wolfman0719 <hsato0719@icloud.com>
2023-07-21 10:45:12 +0900

    Add files via upload

--- FAQ/react/faqpc/src/components/RelatedTopics.tsx
+++ FAQ/react/faqpc/src/components/RelatedTopics.tsx
@@ -9,12 +9,12 @@ export const RelatedTopics = (props: any) => {
   return (
     <>
     <p>関連トピック</p>
-    <table><tbody>
+    <table style = {{width: "100%"}}><tbody>
 	
 	  {	  
         reftopics.map((reftopic: any) => (
 		  <tr>
-		  <button style = {{width: "800px",textAlign: "left"}} className = "btn btn-outline-primary" onClick={() => onClickItem(reftopic.split(comma)[0])}>{`${reftopic.split(comma)[0]}:${reftopic.split(comma)[1]}`}</button>
+		  <button style = {{width: "100%",textAlign: "left"}} className = "btn btn-outline-primary" onClick={() => onClickItem(reftopic.split(comma)[0])}>{`${reftopic.split(comma)[0]}:${reftopic.split(comma)[1]}`}</button>
 		  </tr>
     	)
     )}
