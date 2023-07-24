33ceab8ce679a74f9da4893b17aa0119c1685b90
wolfman0719 <hsato0719@icloud.com>
2023-07-24 16:55:49 +0900

    Update Query.tsx

--- FAQ/react/faqsp/src/components/Query.tsx
+++ FAQ/react/faqsp/src/components/Query.tsx
@@ -5,14 +5,12 @@ import { TopicList } from './TopicList';
 
 export const Query = () => {
 
- const [inputtext, setInputText] = useState<any>("");
+ const [inputtext, setInputText] = useState<any>(localStorage.getItem('inputtext'));
     
  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
      localStorage.setItem('inputtext', e.target.value);
      setInputText(e.target.value);
   }
-
- useEffect( () => {localStorage.getItem('inputtext') && setInputText(localStorage.getItem('inputtext'))},[inputtext]);   
 
   return (
     <>
