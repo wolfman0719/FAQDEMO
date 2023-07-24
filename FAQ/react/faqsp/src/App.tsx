927fdebae72818c5e579923dcf4b7c65f8b013c2
wolfman0719 <hsato0719@icloud.com>
2023-07-24 17:34:08 +0900

    Add files via upload

--- FAQ/react/faqsp/src/App.tsx
+++ FAQ/react/faqsp/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 import { TopicContent } from './components/TopicContent';
 import { Home } from './components/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => {
  
+  useEffect( () => {localStorage.setItem('inputtext','');}, []);   
+
   return (
     <BrowserRouter>
 	<Routes>
