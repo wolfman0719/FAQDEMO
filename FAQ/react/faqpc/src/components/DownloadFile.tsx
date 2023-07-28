7de8ad7201bed82563cf1dc7952d99d5f21e2ae5
wolfman0719 <hsato0719@icloud.com>
2023-07-28 10:09:56 +0900

    Add files via upload

--- FAQ/react/faqpc/src/components/DownloadFile.tsx
+++ FAQ/react/faqpc/src/components/DownloadFile.tsx
@@ -6,7 +6,7 @@ const {fileflag, response} = props;
     
   return (
     <>
-    {fileflag && <a href = {response.DownloadFile}>添付ファイル<i className="bi bi-download fs-4 text-info bg-white p-2"></i></a>}
+    {fileflag && <a className="btn btn-secondary" href = {response.DownloadFile}>添付ファイル<i className="bi bi-download fs-4"></i></a>}
     </>	
   );	
 }
