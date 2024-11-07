a3f8e20ddf4a7958cd40ab1129d8af7722df1c58
wolfman0719 <hsato0719@icloud.com>
2024-11-07 15:18:10 +0900

    Add files via upload

--- FAQ/react/faqsp/src/components/TopicContent.tsx
+++ FAQ/react/faqsp/src/components/TopicContent.tsx
@@ -32,75 +32,34 @@ hljs.registerLanguage('csharp', csharp);
 
 export const TopicContent = (props: any) => {
 
-const {topicid} = useParams();
-
-const navigate = useNavigate();
-
-const [isLoading, setIsLoading] = useState(false);
-const [isError, setIsError] = useState(false);
-const [response, setResponse] = useState<any>("");
-const [errortext, setErrorText] = useState<any>("");
+	const {response} = props; 
   
-const ServerAddress = configinfo.ServerAddress;
-const ServerPort = configinfo.ServerPort;
-const Username = configinfo.Username;
-const Password = configinfo.Password;
-const ApplicationName = configinfo.ApplicationName;
-const Protocol = configinfo.Protocol;
-
- useEffect( () => {
-
-	hljs.initHighlighting();
-	// hljs.initHighlighting.called = false;
-
-	hljs.addPlugin({
-		'after:highlightElement': ({ el, result }) => {
-		  // result の language プロパティが undefined でなければ
-		  if(result.language) {
-			// language を result から取得して code 要素（el）の data-language 属性に設定
-		  if (result.language === 'cos') {
-			el.setAttribute('data-language','ObjectScript');
-		  }
-		  else {
-			el.setAttribute('data-language',result.language); 
-		  }
-		  }
+   useEffect(() => {
+	 hljs.initHighlighting();
+	 // hljs.initHighlighting.called = false;
+  
+	 hljs.addPlugin({
+	  'after:highlightElement': ({ el, result }) => {
+		// result の language プロパティが undefined でなければ
+		if(result.language) {
+		  // language を result から取得して code 要素（el）の data-language 属性に設定
+		if (result.language === 'cos') {
+		  el.setAttribute('data-language','ObjectScript');
 		}
-	   });
-
-	setIsLoading(true);
-	setIsError(false);
-
-	axios
-	  .get<any>(`${Protocol}://${ServerAddress}:${ServerPort}${ApplicationName}/TopicGetById/${topicid}?IRISUsername=${Username}&IRISPassword=${Password}`)
-	  .then((result: any) => {
-	    setResponse(result.data);
-	    hljs.initHighlighting();
-	  })
-      .catch((error: any) => {
-	     setIsError(true)
-		 if (error.response) {			
-		   setErrorText(error.response.data.summary);
-		 }
-		 else if (error.request) {
-		   setErrorText(error.request);
-		 } 
-		 else {
-		   setErrorText(error.message);
-		 }
-	  })
-	  // eslint-disable-next-line react-hooks/exhaustive-deps
-      .finally(() => setIsLoading(false));}, []);   
-        
-  return (
-    <>
-    <button className = "btn btn-light" onClick={() => {navigate(-1)}}><i className="bi bi-caret-left-fill"></i></button>
-    {isLoading && (<p>Data Loading</p>)}
-	{isError && <p style={{ color: "red" }}>エラーが発生しました　{`${errortext}`}</p>}
-	<div style = {{ width: "100%",height: "15%",overflow: "auto",border: "solid #000000 1px"}}><i className="bi bi-hand-index bg-light fs-2"></i><span className="text-primary fs-4" style = {{ marginLeft: "20px", marginRight: "20px"}}>{response.Title}</span><p className="text-info fs-4" style = {{ float: "right", marginRight: "20px"}}>{response.VersionRange}</p></div>
-    {(response.DCURL === "") && <div style = {{ marginLeft: "20px", marginRight: "20px"}}><span dangerouslySetInnerHTML={{__html: response.Description}}></span></div>}	
-    {(response.DCURL !== "") && (response.DCURL !== undefined ) && <div style = {{ marginLeft: "20px", marginRight: "20px"}}><span><p>最新内容は、デベロッパーコミュニティをご参照ください</p><a href={response.DCURL}  target="_blank" rel="noreferrer">デベロッパーコミュニティの記事</a></span></div>}	
-    </>	
-  );	
-}
+		else {
+		  el.setAttribute('data-language',result.language); 
+		}
+		}
+	  }
+	 });
+	 
+	 });
+		  
+	return (
+	  <>
+	  {(response.DCURL === "") && <div style = {{ marginLeft: "20px", marginRight: "20px"}}><span dangerouslySetInnerHTML={{__html: response.Description}}></span></div>}	
+	  {(response.DCURL !== "") && (response.DCURL !== undefined ) && <div style = {{ marginLeft: "20px", marginRight: "20px"}}><span><p>最新内容は、デベロッパーコミュニティをご参照ください</p><a href={response.DCURL}  target="_blank" rel="noreferrer">デベロッパーコミュニティの記事</a></span></div>}	
+	  </>	
+	);	
+  }
 export default TopicContent;
---
+++ FAQ/react/faqsp/src/components/TopicInfo.tsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import { TopicContent } from './TopicContent';
+import { RelatedTopics } from './RelatedTopics';
+import { DownloadFile } from './DownloadFile';
+import { useWindowSize } from "../hooks/useWindowSize";
+import configinfo from '../serverconfig.json';
+
+export const TopicInfo = (props: any) => {
+
+const {topicid} = useParams();
+
+const navigate = useNavigate();
+
+const [isLoading, setIsLoading] = useState(false);
+const [isError, setIsError] = useState(false);
+const [response, setResponse] = useState<any>("");
+const [fileflag, setFileFlag] = useState(false);
+const [reftopics, setRefTopics] = useState<any>([]);
+const [errortext, setErrorText] = useState<any>("");
+  
+const ServerAddress = configinfo.ServerAddress;
+const ServerPort = configinfo.ServerPort;
+const Username = configinfo.Username;
+const Password = configinfo.Password;
+const ApplicationName = configinfo.ApplicationName;
+const Protocol = configinfo.Protocol;
+
+const onClickItem2 = (topicid: any) => {
+	setIsLoading(true);
+	setIsError(false);
+	
+	axios
+	  .get<any>(`${Protocol}://${ServerAddress}:${ServerPort}${ApplicationName}/TopicGetById/${topicid}?IRISUsername=${Username}&IRISPassword=${Password}`)
+	  .then((result: any) => {
+	    setResponse(result.data);
+		if (result.data.FileFlg) {
+			setFileFlag(true);
+		}
+		else {
+			setFileFlag(false);
+		}
+		setRefTopics(result.data.RefArray);
+	  })
+      .catch((error: any) => {
+	     setIsError(true)
+		 if (error.response) {			
+		   setErrorText(error.response.data.summary);
+		 }
+		 else if (error.request) {
+		   setErrorText(error.request);
+		 } 
+		 else {
+		   setErrorText(error.message);
+		 }
+
+	  })
+      .finally(() => setIsLoading(false))
+  };
+
+  useEffect( () => {
+
+	setIsLoading(true);
+	setIsError(false);
+
+	axios
+	  .get<any>(`${Protocol}://${ServerAddress}:${ServerPort}${ApplicationName}/TopicGetById/${topicid}?IRISUsername=${Username}&IRISPassword=${Password}`)
+	  .then((result: any) => {
+	    setResponse(result.data);
+		if (result.data.FileFlg) {
+			setFileFlag(true);
+		}
+		else {
+			setFileFlag(false);
+		}
+		setRefTopics(result.data.RefArray);
+	  })
+      .catch((error: any) => {
+	     setIsError(true)
+		 if (error.response) {			
+		   setErrorText(error.response.data.summary);
+		 }
+		 else if (error.request) {
+		   setErrorText(error.request);
+		 } 
+		 else {
+		   setErrorText(error.message);
+		 }
+	  })
+	  // eslint-disable-next-line react-hooks/exhaustive-deps
+      .finally(() => setIsLoading(false));}, []);   
+
+	  const [,height] = useWindowSize();
+        
+  return (
+    <>
+	<button className = "btn btn-light" onClick={() => {navigate(-1)}}><i className="bi bi-caret-left-fill"></i></button>
+	{isError && <p style={{ color: "red" }}>エラーが発生しました　{`${errortext}`}</p>}
+	<div style = {{ width: "100%",height: "15%",overflow: "auto",border: "solid #000000 1px"}}><i className="bi bi-hand-index bg-light fs-2"></i><span className="text-primary fs-4" style = {{ marginLeft: "20px", marginRight: "20px"}}>{response.Title}</span><p className="text-info fs-4" style = {{ float: "right", marginRight: "20px"}}>{response.VersionRange}</p></div>
+    <div id="topiccontent" style = {{ width: "100%",height: `${height*0.60}px`,overflow: "auto",border: "solid #000000 1px"}}>
+    <TopicContent response = {response} />
+    </div>
+    <div id="topiccontent" style = {{ width: "1000%",height: `${height*0.05}px`,overflow: "auto",border: "solid #000000 1px"}}>
+    <div><p className="text-primary">該当する製品: <span className="text-black">{response.ProductText}</span></p></div>
+    </div>
+    <div id="relatedtopics" style = {{ width: "100%",height: `${height*0.2}px`,overflow: "auto",border: "solid #000000 1px"}}>
+    <RelatedTopics reftopics = {reftopics} onClickItem = {onClickItem2} />
+    </div>
+    <div id="downloadfile" style = {{ width: "100%",height: `${height*0.06}px`,overflow: "auto",border: "solid #000000 1px"}}>
+	<DownloadFile fileflag = {fileflag} response = {response} />
+    </div>	
+    </>	
+  );	
+}
+export default TopicInfo;
