6191e28359bda17b5e26937fbb310dce00cecc4c
wolfman0719 <hsato0719@icloud.com>
2024-11-12 16:33:19 +0900

    Update Header.tsx

--- FAQ/react/faqdirect/src/components/Header.tsx
+++ FAQ/react/faqdirect/src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
     
   return (
     <>
-    <a href="https://www.intersystems.co.jp" ><img src="https://www.intersystems.com/assets/intersystems-logo.png" alt="InterSystems Corporation" style = {{width: "200",height: "50",border: "0"}}/></a>
+    <a href="https://www.intersystems.com/jp" ><img src="https://www.intersystems.com/assets/intersystems-logo.png" alt="InterSystems Corporation" style = {{width: "200",height: "50",border: "0"}}/></a>
 	<p className="fs-1 text-primary"> FAQ検索（よくあるご質問）</p>
     </>	
   );	
