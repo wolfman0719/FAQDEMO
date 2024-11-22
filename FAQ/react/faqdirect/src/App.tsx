4633d01cea364f39c6675aa3f69edfea5b6d44e0
wolfman0719 <hsato0719@icloud.com>
2024-11-22 14:46:18 +0900

    Add files via upload

--- FAQ/react/faqdirect/src/App.tsx
+++ FAQ/react/faqdirect/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Home } from './components/Home';
+import { Top } from './components/Top';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => {
  
   return (
-    <BrowserRouter>
+  <BrowserRouter>
 	<Routes>
-    <Route path="/Home/:topicid"  element={<Home/>} />
+  <Route path="/Home/:topicid"  element={<Home/>} />
+  <Route path="/"  element={<Top/>} />
+  <Route path="/HowTo"  element={<Top/>} />
 	</Routes>
    </BrowserRouter>   
   );	
