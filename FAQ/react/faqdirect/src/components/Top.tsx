918b8eea0d165ff5866734dfd19228117476f298
wolfman0719 <hsato0719@icloud.com>
2024-11-25 17:22:40 +0900

    Update Top.tsx

--- FAQ/react/faqdirect/src/components/Top.tsx
+++ FAQ/react/faqdirect/src/components/Top.tsx
@@ -4,7 +4,7 @@ export const Top = (props: any) => {
     
   return (
     <>
-	<h1>直接指定の方法: http://localhost:3000/Content/:topicid</h1>
+	<h1>直接指定の方法: http://localhost:3000/#/Content/:topicid</h1>
     </>	
   );	
 }
