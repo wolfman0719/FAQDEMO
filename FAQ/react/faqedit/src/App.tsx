f417cc6ea73984f22494473e8bb55677c1acd5d9
wolfman0719 <hsato0719@icloud.com>
2026-05-29 14:12:03 +0900

    Add files via upload

--- FAQ/react/faqedit/src/App.tsx
+++ FAQ/react/faqedit/src/App.tsx
@@ -1,196 +1,37 @@
 import React from 'react';
-import axios from "axios";
-import { useState, useEffect, useMemo, useCallback } from "react";
-import { Header } from './components/Header';
-import { Query } from './components/Query';
-import { TopicList } from './components/TopicList';
-import { TopicContent } from './components/TopicContent';
-import { RelatedTopics } from './components/RelatedTopics';
-import { DownloadFile } from './components/DownloadFile';
-import { useWindowSize } from "./hooks/useWindowSize";
+import { useEffect } from "react";
+import { TopicInfo } from './components/TopicInfo';
+import { TopicEditor } from './components/TopicEditor';
+import { Home } from './components/Home';
+import Signin from "./components/Signin";
+import NoSignin from "./components/NoSignin";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import configinfo from './serverconfig.json';
+import './Table.css'
 
 export const App = () => {
-
-  const [topicList, setTopicList] = useState<any>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [response, setResponse] = useState<any>("");
-  const [reftopics, setRefTopics] = useState<any>([]);
-  const [errortext, setErrorText] = useState<any>("");
-  const [fileflag, setFileFlag] = useState(false);
-  const [prevtopicid, setPrevTopicId] = useState(0);
-  const [prevtopicflag, setPrevTopicFlag] = useState(false);
-  
-  const ServerAddress = configinfo.ServerAddress;
-  const ServerPort = configinfo.ServerPort;
-  const Username = configinfo.Username;
-  const Password = configinfo.Password;
-  const ApplicationName = configinfo.ApplicationName;
-  const Protocol = configinfo.Protocol;
-  
-  const onClickFetchTopicList = (keyword: any) => {
-	
-	setIsLoading(true);
-    setIsError(false);
-  
-	axios
-	  .get<any>(`${Protocol}://${ServerAddress}:${ServerPort}${ApplicationName}/TopicSearchByKeyword/z${keyword}?IRISUsername=${Username}&IRISPassword=${Password}`)
-	  .then((result: any) => {
-	  const topics = result.data.map((topic: any) => ({
-		id: topic.id,
-		title: topic.title
-      }));
-      setTopicList(topics);
-	  })
-      .catch((error: any) => {
-        setIsError(true)
-		 if (error.response) {			
-		   setErrorText(error.response.data.summary);
-		 }
-		 else if (error.request) {
-		   setErrorText(error.request);
-		 } 
-		 else {
-		   setErrorText(error.message);
-		 }
-
-	  })
-      .finally(() => setIsLoading(false));
-  };
-  
-   const onClickItem = useCallback((topicid: any) => {
-	setIsLoading(true);
-	setIsError(false);
-	setPrevTopicFlag(false);
-	setPrevTopicId(topicid);
-
-	axios
-	   // eslint-disable-next-line
-	  .get<any>(`${Protocol}://${ServerAddress}:${ServerPort}${ApplicationName}/TopicGetById/${topicid}?IRISUsername=${Username}&IRISPassword=${Password}`)
-	  .then((result: any) => {
-	    setResponse(result.data);
-		if (result.data.FileFlg) {
-			setFileFlag(true);
-		}
-		else {
-			setFileFlag(false);
-		}
-		setRefTopics(result.data.RefArray);
-	  })
-      .catch((error: any) => {
-	     setIsError(true)
-		 if (error.response) {			
-		   setErrorText(error.response.data.summary);
-		 }
-		 else if (error.request) {
-		   setErrorText(error.request);
-		 } 
-		 else {
-		   setErrorText(error.message);
-		 }
-
-	  })
-      .finally(() => setIsLoading(false))
-  // eslint-disable-next-line
-  }, []);
-
-   const onClickItem2 = (topicid: any) => {
-	setIsLoading(true);
-	setIsError(false);
-	setPrevTopicFlag(true);
-	
-	axios
-	  .get<any>(`${Protocol}://${ServerAddress}:${ServerPort}${ApplicationName}/TopicGetById/${topicid}?IRISUsername=${Username}&IRISPassword=${Password}`)
-	  .then((result: any) => {
-	    setResponse(result.data);
-		if (result.data.FileFlg) {
-			setFileFlag(true);
-		}
-		else {
-			setFileFlag(false);
-		}
-		setRefTopics(result.data.RefArray);
-	  })
-      .catch((error: any) => {
-	     setIsError(true)
-		 if (error.response) {			
-		   setErrorText(error.response.data.summary);
-		 }
-		 else if (error.request) {
-		   setErrorText(error.request);
-		 } 
-		 else {
-		   setErrorText(error.message);
-		 }
-
-	  })
-      .finally(() => setIsLoading(false))
-  };
-  
-  const [,height] = useWindowSize();
-
+ 
   useEffect( () => {
-
-	setIsLoading(true);
-    setIsError(false);
+    localStorage.setItem('inputtext','');
+    localStorage.removeItem('topicid');
+    localStorage.removeItem('username');
+    localStorage.removeItem('password');
+    localStorage.removeItem('edit');
+  }, []); 
   
-	axios
-	  .get<any>(`${Protocol}://${ServerAddress}:${ServerPort}${ApplicationName}/TopicSearchByNew?IRISUsername=${Username}&IRISPassword=${Password}`)
-	  .then((result: any) => {
-	  const topics = result.data.map((topic: any) => ({
-		id: topic.id,
-		title: topic.title
-      }));
-      setTopicList(topics);
-	  })
-      .catch((error: any) => {
-        setIsError(true)
-		setErrorText(error.message);
-	  })
-      .finally(() => setIsLoading(false));
-      
-	  // eslint-disable-next-line
-      }, []);
-  
-    // eslint-disable-next-line
-    const TopicListMemo = useMemo(() => <TopicList isLoading = {isLoading} topicList = {topicList} onClickItem = {onClickItem} />, [onClickItem,topicList]);
+  const edit = configinfo.Edit
 
-    const cardStyle: React.CSSProperties = { backgroundColor: "white", borderRadius: "12px" };
-
-    return (
-    <>
-    <div className="title" style={{ ...cardStyle, padding: "12px 16px", marginBottom: "8px" }}>
-      <Header />
-    </div>
-    <div className="query" style={{ ...cardStyle, padding: "12px 16px", marginBottom: "8px" }}>
-      <Query onClickItem = {onClickItem} onClickFetchTopicList = {onClickFetchTopicList} />
-      {prevtopicflag  ? (<button className="btn grey lighten-1 white-text waves-effect waves-light" style={{ borderRadius: "24px" }} onClick={() => onClickItem(prevtopicid)}><i className="material-icons left">arrow_back</i>前のトピックに戻る</button>):
-      (<button  className="btn grey lighten-1 white-text waves-effect waves-light disabled" style={{ borderRadius: "24px" }} onClick={() => onClickItem(prevtopicid)} disabled><i className="material-icons left">arrow_back</i>前のトピックに戻る</button>)}
-      {isError && <p style={{ color: "red" }}>エラーが発生しました　{`${errortext}`}</p>}
-    </div>
-    <div className="topiclist" style={{ ...cardStyle, float: "left", width: "calc(40% - 4px)", height: `${height*0.81}px`, overflow: "auto", marginRight: "8px" }}>
-      {TopicListMemo}
-    </div>
-    <div style={{ width: "calc(60% - 4px)", height: `${height*0.81}px`, display: "flex", flexDirection: "column", gap: "8px" }}>
-      <div id="topiccontent" style={{ ...cardStyle, flex: 5, overflow: "auto" }}>
-        <span className="blue-text text-darken-2" style={{ marginLeft: "20px", marginRight: "20px", fontSize: "1.25rem" }}><img src="./images/Question.gif" alt="Question"/> {response.Title}</span>
-        <p className="teal-text" style={{ float: "right", marginRight: "20px", fontSize: "1.5rem" }}>{response.VersionRange}</p>
-      </div>
-      <div id="topiccontent" style={{ ...cardStyle, flex: 45, overflow: "auto" }}>
-        <TopicContent response = {response} />
-      </div>
-      <div id="topiccontent" style={{ ...cardStyle, flex: 5, overflow: "auto" }}>
-        <p className="blue-text text-darken-2" style={{ margin: "4px 16px" }}>該当する製品: <span className="black-text">{response.ProductText}</span></p>
-      </div>
-      <div id="relatedtopics" style={{ ...cardStyle, flex: 20, overflow: "auto" }}>
-        <RelatedTopics reftopics = {reftopics} onClickItem = {onClickItem2} />
-      </div>
-      <div id="downloadfile" style={{ ...cardStyle, flex: 6, overflow: "auto" }}>
-        <DownloadFile fileflag = {fileflag} response = {response} />
-      </div>
-    </div>
-    </>
+  return (
+  <BrowserRouter basename = {'/faqedit/'}>
+	<Routes>
+    {edit ? <Route path='/' element={<Signin />} />
+  : <Route path='/' element={<NoSignin />} />}
+  <Route path='/Home' element={<Home />} />
+  <Route path="/Content/:topicid"  element={<TopicInfo direct={false}/>} />
+  <Route path="/Direct/:topicid"  element={<TopicInfo direct={true}/>} />
+  {edit &&<Route path="/Edit/:topicid"  element={<TopicEditor />} />}
+	</Routes>
+  </BrowserRouter>   
   );	
 }
 export default App;
