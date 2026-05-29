2207267a93b121fa75daa7284a416f586e71e19a
wolfman0719 <hsato0719@icloud.com>
2026-05-29 14:00:50 +0900

    Add files via upload

--- FAQ/react/faqedit/src/index.tsx
+++ FAQ/react/faqedit/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap-icons/font/bootstrap-icons.css";
+import 'materialize-css/dist/css/materialize.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,4 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
