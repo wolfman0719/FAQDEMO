cc707176223c2b042633abf61628ddd710b1e556
wolfman0719 <hsato0719@icloud.com>
2026-06-02 14:39:27 +0900

    Add files via upload

--- FAQ/react/faqdirect/src/App.tsx
+++ FAQ/react/faqdirect/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Home } from './components/Home';
 import { Top } from './components/Top';
+import { Second } from './components/Second';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => {
@@ -8,8 +9,10 @@ export const App = () => {
   return (
   <BrowserRouter basename="/faqdirect/">
 	<Routes>
-  <Route path="/"  element={<Top/>} />  
+  <Route path="/"  element={<Top/>} />
+  <Route path="/Second/"  element={<Second/>} />  
   <Route path="/Content/:topicid/"  element={<Home/>} />
+  <Route  element={<Second/>} />
 	</Routes>
    </BrowserRouter>   
   );	
--- FAQ/react/faqdirect/src/index.tsx
+++ FAQ/react/faqdirect/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap-icons/font/bootstrap-icons.css";
+import 'materialize-css/dist/css/materialize.min.css';
+import './global.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
