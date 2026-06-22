d11e90a0acdb78cba6579f03dd659aa429bb7cfe
wolfman0719 <hsato0719@icloud.com>
2026-06-22 16:53:08 +0900

    Add files via upload

--- FAQ/react/faqedit/src/components/Home.tsx
+++ FAQ/react/faqedit/src/components/Home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Header } from './Header';
 import { Query } from './Query';
 import { QueryById } from './QueryById';
-import { VectorSearchQuery } from './VectorSearchQuery';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { useLocation } from "react-router-dom";
@@ -14,8 +13,6 @@ export const Home = () => {
   // eslint-disable-next-line
   const editflag = (location.state.edit as unknown) == 1 ? true : false;
 
-  const vectorSearch = configinfo.VectorSearch;
-
   localStorage.setItem('username', location.state.username);
   localStorage.setItem('password', location.state.password);
   localStorage.setItem('edit', location.state.edit);
@@ -26,7 +23,6 @@ export const Home = () => {
     <TabList>
     <Tab>キーワード検索</Tab>
     <Tab>ID検索</Tab>
-    {vectorSearch && <Tab>ベクトル検索</Tab>}
     </TabList>
 
     <TabPanel>
@@ -46,16 +42,6 @@ export const Home = () => {
         <QueryById />
       </div>
     </TabPanel>
-
-    {vectorSearch && <TabPanel>
-      <div className="faq-card" style={{margin: "8px"}}>
-        <Header />
-      </div>
-      <div className="faq-card" style={{margin: "8px"}}>
-        <VectorSearchQuery username={location.state.username} password={location.state.password} edit={editflag} />
-      </div>
-    </TabPanel>}
-
     </Tabs>
     </>
   );
