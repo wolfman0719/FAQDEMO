85cc192e3c9a094957554348cb88fa69aa4acd93
wolfman0719 <hsato0719@icloud.com>
2026-06-30 16:27:19 +0900

    Add files via upload

--- FAQ/react/faqdirect/src/components/Header.tsx
+++ FAQ/react/faqdirect/src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
     
   return (
     <div style={{display: "flex", alignItems: "center", gap: "16px"}}>
-      <a href="https://www.intersystems.com/jp"><img src="https://www.intersystems.com/assets/intersystems-logo.png" alt="InterSystems Corporation" style={{width: "200", height: "50", border: "0"}}/></a>
+      <a href="https://www.intersystems.com/jp" style={{outline: "none"}}><img src="https://www.intersystems.com/assets/intersystems-logo.png" alt="InterSystems Corporation" style={{width: "200", height: "50", border: "none", display: "block"}}/></a>
       <p className="blue-text" style={{margin: 0, fontSize: "2rem", fontWeight: 500}}> FAQ検索（よくあるご質問）</p>
     </div>
   );	
