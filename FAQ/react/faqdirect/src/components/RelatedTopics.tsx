c88a209d0493f3557b8a181ff654b8b2c91d40e3
wolfman0719 <hsato0719@icloud.com>
2026-07-07 09:22:03 +0900

    Add files via upload

--- FAQ/react/faqdirect/src/components/RelatedTopics.tsx
+++ FAQ/react/faqdirect/src/components/RelatedTopics.tsx
@@ -23,7 +23,8 @@ export const RelatedTopics = (props: any) => {
 		  <tr key={index} style={getRowStyle(index)}
 		      onMouseEnter={() => setHoveredIndex(index)}
 		      onMouseLeave={() => setHoveredIndex(null)}>
-		  <td><button style={{width: "100%", textAlign: "left", color: "#000000", background: "transparent", border: "none", padding: "6px 8px"}} onClick={() => onClickItem(reftopic.split(comma)[0])}>{`${reftopic.split(comma)[0]}:${reftopic.split(comma)[1]}`}</button></td>
+		  <td style={{padding: "6px 8px", color: "#000000", whiteSpace: "nowrap"}}>{reftopic.split(comma)[0]}</td>
+		  <td style={{padding: "6px 8px", width: "100%"}}><button style={{width: "100%", textAlign: "left", color: "#000000", background: "transparent", border: "none", padding: "0"}} onClick={() => onClickItem(reftopic.split(comma)[0])}>{reftopic.split(comma)[1]}</button></td>
 		  </tr>
     	)
     )}
